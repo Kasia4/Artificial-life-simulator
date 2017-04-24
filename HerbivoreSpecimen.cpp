6d069c78713b209d19240eb95f2efa893168d270
Kasia4 <katarzyna.rybacka4@gmail.com>
2017-04-24 19:39:44 +0200

    Conrete Specimen painting added

---
+++ HerbivoreSpecimen.cpp
@@ -0,0 +1,22 @@
+#include "HerbivoreSpecimen.h"
+
+HerbivoreSpecimen::HerbivoreSpecimen()
+{
+
+}
+
+
+void HerbivoreSpecimen::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    QPainterPath circle_path;
+    circle_path.addEllipse(boundingRect());
+    painter->setPen(QPen(QColor(0, 160, 0),2));
+    painter->setBrush(QColor(0,210,0));
+    painter->drawPath(circle_path);
+}
+
+Specimen* HerbivoreSpecimen::clone() const
+{
+    HerbivoreSpecimen* temp = new HerbivoreSpecimen();
+    return temp;
+}
